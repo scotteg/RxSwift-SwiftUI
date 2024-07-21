4bde505ad51cd085f9df04c1105934f2a818617f
Scott Gardner <scott.gardner@mac.com>
2024-07-21 10:17:34 -0500

    Initial Commit

---
+++ RxSwift+SwiftUI/RxSwift_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RxSwift_SwiftUIApp.swift
+//  RxSwift+SwiftUI
+//
+//  Created by Scott Gardner on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct RxSwift_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
